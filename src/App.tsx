730bc5a24c6b5c9b4ac8fbbc5b5d2d1515e10cff
Janine Luk <janineluk95@gmail.com>
2024-10-08 11:36:08 +0100

    Create Header and Portfolio Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
+import Header from './components/Header/Header';
+import PortfolioGrid from './components/PortfolioGrid/PortfolioGrid';
 import Timeline from './components/Timeline/Timeline';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
       <Timeline />
+      <PortfolioGrid />
     </div>
   );
 }
---
+++ src/components/PortfolioGrid/PortfolioGrid.tsx
@@ -0,0 +1,13 @@
+import './portfoliogrid.css';
+
+const PortfolioGrid: React.FC = () => {
+        return (
+          <div className="container">
+            <div className="portfolio-grid">
+              <h1>hello world</h1>
+            </div>
+          </div>
+        );
+}
+
+export default PortfolioGrid;
